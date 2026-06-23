1a41b3eb94800f33d93db0b5e75b86434e5dd069
InkFlow Dev <rveigarangel@gmail.com>
2026-06-23 18:53:24 -0300

    Use user-provided PNG logo with transparent background

--- src/components/ui/logo.tsx
+++ src/components/ui/logo.tsx
@@ -1,19 +1,14 @@
 import { cn } from "@/lib/utils"
 
 export function InkagendaIcon({ className }: { className?: string }) {
-  return (
-    // eslint-disable-next-line @next/next/no-img-element
-    <img src="/logo.svg" alt="inkagenda" className={className} />
-  )
+  // eslint-disable-next-line @next/next/no-img-element
+  return <img src="/logo.png" alt="inkagenda" className={className} />
 }
 
 export function InkagendaLogo({ className }: { className?: string }) {
   return (
-    <div className={cn("flex flex-col items-center gap-3", className)}>
-      <InkagendaIcon className="w-20" />
-      <span className="text-2xl font-light tracking-widest text-foreground">
-        inkagenda
-      </span>
+    <div className={cn("flex flex-col items-center", className)}>
+      <InkagendaIcon className="w-24" />
     </div>
   )
 }
